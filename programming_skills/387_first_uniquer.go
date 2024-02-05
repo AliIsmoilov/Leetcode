2a517970f7cb39702a4739f4a660682fa005b29e
AliIsmoilov <alimadadismoilov@gmail.com>
2024-02-05 13:53:20 +0500

    387 first unique

---
+++ programming_skills/387_first_uniquer.go
@@ -0,0 +1,31 @@
+package programmingskills
+
+// Input: s = "leetcode"
+// Output: 0
+func isUnique(s string, ch string) bool {
+	count := 0
+	for _, v := range s {
+		if string(v) == ch {
+			count++
+			if count >= 2 {
+				return false
+			}
+		}
+	}
+	if count >= 2 {
+		return false
+	} else {
+		return true
+	}
+}
+
+func FirstUniqChar(s string) int {
+	for i, ch := range s {
+		is_unique := isUnique(s, string(ch))
+		if is_unique {
+			return i
+		}
+	}
+
+	return -1
+}
