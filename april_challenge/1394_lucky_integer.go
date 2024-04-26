f47601b9af7532163e8758be66ec63c45bda5036
AliIsmoilov <alimadadismoilov@gmail.com>
2024-04-26 16:37:39 +0500

    1394. Find Lucky Integer in an Array

---
+++ april_challenge/1394_lucky_integer.go
@@ -0,0 +1,35 @@
+package aprilchallenge
+
+// Example 1:
+
+// Input: arr = [2,2,3,4]
+// Output: 2
+// Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+// Example 2:
+
+// Input: arr = [1,2,2,3,3,3]
+// Output: 3
+// Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+// Example 3:
+
+// Input: arr = [2,2,2,3,3]
+// Output: -1
+// Explanation: There are no lucky numbers in the array.
+
+func FindLucky(arr []int) int {
+	m := make(map[int]int)
+	resp := -1
+
+	for _, num := range arr {
+		m[num]++
+	}
+	// fmt.Println(m)
+
+	for k, v := range m {
+		if k == v && k > resp {
+			resp = k
+		}
+	}
+
+	return resp
+}
